29036c73d1832d79b73c64e7794ae75f3161a7b6
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-08-11 04:28:47 +0000

    自动创建手机热点局域网相关防火墙规则，便于热点机创建网对网访问。在三星fold3测试通过

--- magisk/hotspot_iprule.sh
+++ magisk/hotspot_iprule.sh
@@ -11,13 +11,13 @@ case "$ACTION" in
     iptables -t nat -A tetherctrl_nat_POSTROUTING -s $HOT_CIDR -o $ZT_IFACE -j MASQUERADE
     iptables -A tetherctrl_FORWARD -i $HOT_IFACE -o $ZT_IFACE -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
     iptables -A tetherctrl_FORWARD -i $ZT_IFACE -o $HOT_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
-    echo "[ZT-NAT] Rules applied." >> /data/adb/zerotier/run/daemon.log
+    echo "[ZT-NAT] Rules applied." >> $ZTROOT/run/daemon.log
     ;;
   del)
     iptables -t nat -D tetherctrl_nat_POSTROUTING -s $HOT_CIDR -o $ZT_IFACE -j MASQUERADE
     iptables -D tetherctrl_FORWARD -i $HOT_IFACE -o $ZT_IFACE -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
     iptables -D tetherctrl_FORWARD -i $ZT_IFACE -o $HOT_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
-    echo "[ZT-NAT] Rules removed." >> /data/adb/zerotier/run/daemon.log
+    echo "[ZT-NAT] Rules removed." >> $ZTROOT/run/daemon.log
     ;;
   *)
     echo "Usage: $0 [add|del]"
