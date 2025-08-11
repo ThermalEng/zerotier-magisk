e61faeb38ee0551d68085b3b37f3a4491fb4fe26
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-08-11 12:11:44 +0800

    Create hotspot iprule
    给热点机创建的局域网增加相应的防火墙规则，配合zerotier的路由，实现网连网。

---
+++ hotspot_iprule.sh
@@ -0,0 +1,26 @@
+#!/system/bin/sh
+
+ACTION="$1"  # 参数：add 或 del
+
+ZT_IFACE=$(ip link | awk -F': ' '/\bzt[0-9a-f]+\b/ {print $2; exit}')
+HOT_IFACE=$(ip link | awk -F': ' '/swlan|ap0|softap/ {print $2; exit}')
+HOT_CIDR=$(ip -4 addr show dev "$HOT_IFACE" | awk '/inet /{print $2; exit}')
+
+case "$ACTION" in
+  add)
+    iptables -t nat -A tetherctrl_nat_POSTROUTING -s $HOT_CIDR -o $ZT_IFACE -j MASQUERADE
+    iptables -A tetherctrl_FORWARD -i $HOT_IFACE -o $ZT_IFACE -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+    iptables -A tetherctrl_FORWARD -i $ZT_IFACE -o $HOT_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
+    echo "[ZT-NAT] Rules applied." >> /data/adb/zerotier/run/daemon.log
+    ;;
+  del)
+    iptables -t nat -D tetherctrl_nat_POSTROUTING -s $HOT_CIDR -o $ZT_IFACE -j MASQUERADE
+    iptables -D tetherctrl_FORWARD -i $HOT_IFACE -o $ZT_IFACE -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+    iptables -D tetherctrl_FORWARD -i $ZT_IFACE -o $HOT_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
+    echo "[ZT-NAT] Rules removed." >> /data/adb/zerotier/run/daemon.log
+    ;;
+  *)
+    echo "Usage: $0 [add|del]"
+    exit 1
+    ;;
+esac
