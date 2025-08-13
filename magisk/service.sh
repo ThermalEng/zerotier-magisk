82a5a8bf9223bab9517f17b3795e7b9c72004f4f
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-08-13 11:15:46 +0800

    Update service.sh

--- magisk/service.sh
+++ magisk/service.sh
@@ -47,23 +47,7 @@ inotifyd $MODDIR/handle.sh $PIPE_CLI:w &>/dev/null &  # toybox inotifyd bug: nee
 # ----------------------------------------------
 
 wait_until_login
-
-
-ZT_IFACE=$(ip link | awk -F': ' '/\bzt[0-9a-f]+\b/ {print $2; exit}')  # 也可根据设备具体接口写死
-HOT_IFACE=$(ip link | awk -F': ' '/swlan|ap0|softap/ {print $2; exit}')
-ZT_SCRIPT=$MODDIR/hotspot_iprule.sh
-
-# 判断是否启用热点（检测接口是否有 IP）
-HOT_IP=$(ip -4 addr show "$HOT_IFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
-
-if [ -n "$HOT_IP" ]; then
-  echo "[ZT-NAT] Hotspot detected ($HOT_IFACE: $HOT_IP), applying rules..." >> $ZTROOT/run/daemon.log
-  "$ZT_SCRIPT" add
-else
-  echo "[ZT-NAT] Hotspot not active, skipping rule insertion." >> $ZTROOT/run/daemon.log
-fi
-
-
+$MODDIR/hotspot_iprule.sh add
 
 if [[ -d "$APPROOT" ]]; then
   rm -rf $APPROOT/run
